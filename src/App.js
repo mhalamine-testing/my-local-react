9fa5c7abb33de89f36b32c32d65ea39ba9e36e34
Mo <mo-mini@Mos-Mac-mini.local>
2022-07-15 09:27:39 +0300

    modified item values

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
+      title: "Toilet Paper - Updated",
+      amount: 194.12,
       date: new Date(2020, 7, 14),
     },
     { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
